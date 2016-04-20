2421f43069288773917db9bd24e54da65edfe142
CarolaC <carolachristoffel@gmx.de>
2016-04-20 14:10:37 +0200

    switch between cameras

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraHandler : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera insideCamera;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void SwitchView()
+    {
+       if (mainCamera.enabled)
+       {
+            mainCamera.enabled = false;
+            insideCamera.enabled = true;
+       }
+       else
+       {
+            insideCamera.enabled = false;
+            mainCamera.enabled = true;
+       }
+    }
+}
