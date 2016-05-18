774b82ed6cc90638d0f90dabd669b450ac336efb
CarolaC <carolachristoffel@gmx.de>
2016-05-18 16:39:04 +0200

    started to work on: sound areas visible on canvas

--- Assets/Scripts/SoundAreaSelector.cs
+++ Assets/Scripts/SoundAreaSelector.cs
@@ -7,11 +7,16 @@ using System.Collections.Generic;
 public class SoundAreaSelector : MonoBehaviour {
 
     public Transform listenerTransform;
-    public GameObject soundAreaPrefab;
+	public GameObject soundAreaFloorPrefab;
+	public Canvas canvas;
+	public GameObject soundAreasPanel;
+	public GameObject soundAreaPanelPrefab;
 
     private CaveRectUtility caveRectUtil;
     private List<Rect> soundAreas = new List<Rect>();
-    private List<GameObject> soundAreaPanels = new List<GameObject>();
+	private List<Rect> canvasSoundAreas = new List<Rect>();
+    private List<GameObject> soundAreaFloors = new List<GameObject>();
+	private List<GameObject> soundAreaPanels = new List<GameObject>();
     private List<int> instruments = new List<int>();
 
     [HideInInspector]
@@ -41,7 +46,10 @@ public class SoundAreaSelector : MonoBehaviour {
             instruments.Add(PlayerPrefs.GetInt("Instrument" + i));
         }
 
-        divideRect(caveRectUtil.caveRect);
+		// divide the cave rect into sound areas
+        divideCaveRect(caveRectUtil.caveRect);
+		// divide the panel on the canvas into sound areas
+		dividePanelRect(soundAreasPanel.GetComponent<RectTransform>().rect);
 	}
 	
 	// Update is called once per frame
@@ -52,10 +60,10 @@ public class SoundAreaSelector : MonoBehaviour {
             {
                 if (activeSoundArea != i)
                 {
-                    soundAreaPanels[activeSoundArea].GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+					soundAreaFloors[activeSoundArea].GetComponent<Renderer>().material.color = new Color(0, 0, 0);
                     activeSoundArea = i;
                     activeInstrument = instruments[activeSoundArea];
-                    soundAreaPanels[activeSoundArea].GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+					soundAreaFloors[activeSoundArea].GetComponent<Renderer>().material.color = new Color(255, 0, 0);
                     print("Listener in Area " + activeSoundArea);
                     StartCoroutine(testLightCoroutine());
                     instrumentText.text = "Instrument " + activeInstrument;
@@ -64,46 +72,92 @@ public class SoundAreaSelector : MonoBehaviour {
         }
 	}
     
-    // divide rect into sound areas
-    void divideRect(Rect caveRect)
+    // divide the cave rect into sound areas
+	void divideCaveRect(Rect rect)
     {
         if (soundAreaCount == 4) 
 		{
-			soundAreas.Add (new Rect (caveRect.x, caveRect.y, caveRect.width * 0.5f, caveRect.height * 0.5f));
-			soundAreas.Add (new Rect (caveRect.x + (caveRect.width * 0.5f), caveRect.y, caveRect.width * 0.5f, caveRect.height * 0.5f));
-			soundAreas.Add (new Rect (caveRect.x, caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.5f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.center.x, caveRect.center.y, caveRect.width * 0.5f, caveRect.height * 0.5f));
+			soundAreas.Add (new Rect (rect.x, rect.y, rect.width * 0.5f, rect.height * 0.5f));
+			soundAreas.Add (new Rect (rect.x + (rect.width * 0.5f), rect.y, rect.width * 0.5f, rect.height * 0.5f));
+			soundAreas.Add (new Rect (rect.x, rect.y + (rect.height * 0.5f), rect.width * 0.5f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.center.x, rect.center.y, rect.width * 0.5f, rect.height * 0.5f));
 		}
         else if (soundAreaCount == 6)
         {
-            soundAreas.Add(new Rect(caveRect.x, caveRect.y, caveRect.width * 0.33f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.33f), caveRect.y, caveRect.width * 0.33f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.66f), caveRect.y, caveRect.width * 0.33f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x, caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.33f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.33f), caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.33f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.66f), caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.33f, caveRect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x, rect.y, rect.width * 0.33f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.33f), rect.y, rect.width * 0.33f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.66f), rect.y, rect.width * 0.33f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x, rect.y + (rect.height * 0.5f), rect.width * 0.33f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.33f), rect.y + (rect.height * 0.5f), rect.width * 0.33f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.66f), rect.y + (rect.height * 0.5f), rect.width * 0.33f, rect.height * 0.5f));
         }
         else if (soundAreaCount == 8)
         {
-            soundAreas.Add(new Rect(caveRect.x, caveRect.y, caveRect.width * 0.25f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.25f), caveRect.y, caveRect.width * 0.25f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.5f), caveRect.y, caveRect.width * 0.25f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.75f), caveRect.y, caveRect.width * 0.25f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x, caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.25f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.25f), caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.25f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.5f), caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.25f, caveRect.height * 0.5f));
-            soundAreas.Add(new Rect(caveRect.x + (caveRect.width * 0.75f), caveRect.y + (caveRect.height * 0.5f), caveRect.width * 0.25f, caveRect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x, rect.y, rect.width * 0.25f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.25f), rect.y, rect.width * 0.25f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.5f), rect.y, rect.width * 0.25f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.75f), rect.y, rect.width * 0.25f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x, rect.y + (rect.height * 0.5f), rect.width * 0.25f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.25f), rect.y + (rect.height * 0.5f), rect.width * 0.25f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.5f), rect.y + (rect.height * 0.5f), rect.width * 0.25f, rect.height * 0.5f));
+			soundAreas.Add(new Rect(rect.x + (rect.width * 0.75f), rect.y + (rect.height * 0.5f), rect.width * 0.25f, rect.height * 0.5f));
         }
 
-        GameObject soundAreaPanel;
-        for (int i = 0; i < soundAreas.Count; i++)
-        {
-            print("Sound Area " + i + ": " + soundAreas[i]);
-            soundAreaPanel = (GameObject)Instantiate(soundAreaPrefab, new Vector3(soundAreas[i].center.x, 0, soundAreas[i].center.y), Quaternion.identity);
-            soundAreaPanel.transform.localScale = new Vector3(soundAreas[i].width, 0.1f, soundAreas[i].height);
-            soundAreaPanels.Add(soundAreaPanel);
-        }
+		// create sound area floors
+		GameObject soundAreaFloor;
+		for (int i = 0; i < soundAreas.Count; i++) {
+			print ("Sound Area " + i + ": " + soundAreas [i]);
+			soundAreaFloor = (GameObject)Instantiate (soundAreaFloorPrefab, new Vector3 (soundAreas[i].center.x, 0, soundAreas[i].center.y), Quaternion.identity);
+			soundAreaFloor.transform.localScale = new Vector3 (soundAreas [i].width, 0.1f, soundAreas[i].height);
+			soundAreaFloors.Add (soundAreaFloor);
+		}
     }
+
+	void dividePanelRect(Rect rect)
+	{
+		float width = rect.width * canvas.scaleFactor;
+		float height = rect.height * canvas.scaleFactor;
+
+		if (soundAreaCount == 4) 
+		{
+			canvasSoundAreas.Add (new Rect (rect.x, rect.y, width * 0.5f, height * 0.5f));
+			canvasSoundAreas.Add (new Rect (rect.x + (width * 0.5f), rect.y, width * 0.5f, height * 0.5f));
+			canvasSoundAreas.Add (new Rect (rect.x, rect.y + (height * 0.5f), width * 0.5f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.center.x, rect.center.y, width * 0.5f, height * 0.5f));
+		}
+		else if (soundAreaCount == 6)
+		{
+			canvasSoundAreas.Add(new Rect(rect.x, rect.y, width * 0.33f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.33f), rect.y, width * 0.33f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.66f), rect.y, width * 0.33f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x, rect.y + (rect.height * 0.5f), width * 0.33f, rect.height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.33f), rect.y + (height * 0.5f), width * 0.33f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.66f), rect.y + (height * 0.5f), width * 0.33f, height * 0.5f));
+		}
+		else if (soundAreaCount == 8)
+		{
+			canvasSoundAreas.Add(new Rect(rect.x, rect.y, width * 0.25f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.25f), rect.y, width * 0.25f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.5f), rect.y, width * 0.25f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.75f), rect.y, width * 0.25f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x, rect.y + (height * 0.5f), width * 0.25f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.25f), rect.y + (height * 0.5f), width * 0.25f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.5f), rect.y + (height * 0.5f), width * 0.25f, height * 0.5f));
+			canvasSoundAreas.Add(new Rect(rect.x + (width * 0.75f), rect.y + (height * 0.5f), width * 0.25f, height * 0.5f));
+		}
+
+		GameObject soundAreaPanel;
+		for (int i = 0; i < canvasSoundAreas.Count; i++) {
+			soundAreaPanel = (GameObject)Instantiate (soundAreaPanelPrefab);
+			soundAreaPanel.transform.SetParent(soundAreasPanel.transform, false);
+			soundAreaPanel.transform.localPosition = new Vector3 (canvasSoundAreas[i].x, canvasSoundAreas[i].y, 0);
+			float panelWidth = soundAreaPanel.GetComponent<RectTransform> ().rect.width;
+			panelWidth = canvasSoundAreas[i].width;
+			float panelHeight = soundAreaPanel.GetComponent<RectTransform> ().rect.height;
+			panelHeight = canvasSoundAreas[i].height;
+			soundAreaPanels.Add(soundAreaPanel);
+		}
+	}
 
     private IEnumerator testLightCoroutine()
     {
